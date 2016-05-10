c9464c99c3a92f06fe8c8508bfa97e507ca10087
Drew Holt <drew@liatrio.com>
2016-05-10 11:52:03 -0700

    RP-40 create jenkins job AutomationManagementJob

--- files/default/test/default_test.rb
+++ files/default/test/default_test.rb
@@ -10,8 +10,8 @@ end
 
 describe_recipe 'jenkins-as-code::default' do
 
-  it "listens for http on tcp port 80" do
-    service_is_listening("80", "httpd")
+  it "listens for http on tcp port 8080" do
+    service_is_listening("8080", "httpd")
   end
 
   #it 'must match the web check Apache' do
