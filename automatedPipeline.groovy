cc078f64dee4c2917f7ec815e6614ff9d4f980ab
e547866 <robert@Roberts-MacBook-Pro-2.local>
2016-05-13 21:38:40 -0500

    Adding features to disable on dev box and create builds for all branches when run

--- automatedPipeline.groovy
+++ automatedPipeline.groovy
@@ -3,8 +3,9 @@ import java.io.FileReader;
 import java.nio.file.*
 import groovy.json.*
 
-
-GIT_URL = "https://github.com/"
+DEV_BOX = "true"
+GIT_API = "https://api.github.com/repos/"
+GIT_URL= "https://github.com/"
 String fileName = "buildDeployPipelines.json"
 def file = readFileFromWorkspace(fileName)
 def inputJson = new JsonSlurper().parseText(file)
@@ -21,6 +22,10 @@ for( component in components ) {
 def createDeployJob(productName, projectName, environment) {
   def deployJobName = createDeployJobName(projectName, productName, environment)
   job(deployJobName) {
+    if(DEV_BOX)
+    {
+      disabled()
+    }
     description("<h3>This job was created by automation.  Manual edits to this job are discouraged.</h3> ")
     steps {
       label('master')
@@ -28,42 +33,53 @@ def createDeployJob(productName, projectName, environment) {
   }
   return deployJobName
 }
+
 def createDeployJobName(projectName, productName , environment) {
-  return projectName + "-" + productName +"-deploy-" + environment
+  return (projectName + "-" + productName +"-deploy-" + environment).toLowerCase()
+}
+
+def getBranches(branchApi) {
+  def auth = "ca5150b27ad3138cf12c6a5e9c24dd5b88b926b5"
+  def json = new JsonSlurper()
+  return json.parse(branchApi.toURL().newReader(requestProperties: ["Authorization": "token ${auth}".toString(), "Accept": "application/json"]))
 }
 
 def createBuildJob(component) {
-  String branch = component.branch? component.branch : 'master'
-  String buildSuffix = branch == 'master'? "-build" : "-" + branch.replaceAll('/','-') + "-build"
-  String buildJobName = component.scmProject + "-" + component.productName + buildSuffix
-  String gitUrl =  GIT_URL + component.scmProject + "/" + component.productName + ".git"
+  String branchApi =  GIT_API + component.scmProject + "/" + component.productName + "/branches"
+  String repoUrl = GIT_URL + component.scmProject + "/" + component.productName
   def ciEnvironments = component.ciEnvironments
   def downStreamJobs = []
   for(env in ciEnvironments)
     downStreamJobs.add( createDeployJobName(component.scmProject, component.productName, env) )
 
-  mavenJob(buildJobName) {
-    description("<h3>This job was created with automation.  Manual edits to this job are discouraged.</h3> ")
-    label('master')
-    scm {
-      git(gitUrl, branch)
-    }
-    triggers {
-      scm('H/2 * * * *')
-    }
-    mavenInstallation('maven 3')
-    goals("clean package")
-     postBuildSteps('SUCCESS') {
-      environmentVariables {
-        //propertiesFile('propsfile') Add propertiesFile when available
-      }
-      if(downStreamJobs) {
-        downstreamParameterized {
-          trigger(downStreamJobs.join(", ")) {
-          }
+  def branches = getBranches(branchApi)
+  branches.each {
+        def branchName = it.name
+        def jobName = "${component.scmProject}-${component.productName}-${branchName}-build".replaceAll('/','-').toLowerCase()
+        mavenJob(jobName) {
+            if(DEV_BOX)
+            {
+              disabled()
+            }
+            description("<h3>This job was created with automation.  Manual edits to this job are discouraged.</h3> ")
+            scm {
+                git(repoUrl, branchName)
+            }
+            triggers {
+              scm('H/2 * * * *')
+            }
+            mavenInstallation('maven 3')
+            goals("clean deploy")
+             postBuildSteps('SUCCESS') {
+
+              if(downStreamJobs && (branchName == "master")) {
+                downstreamParameterized {
+                  trigger(downStreamJobs.join(", ")) {
+                  }
+                }
+              }
+            }
         }
-      }
+      return jobName
     }
-  }
-  return buildJobName
 }
