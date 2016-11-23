425861bfb218cad77e8c9322c1e4dba4b8db9cb8
Drew Holt <drew@liatrio.com>
2016-11-23 11:47:34 -0700

    LTA-42 THEIA-117 updated wrapper recipes to use jenkins-liatrio, updated .kitchen.yml to match Vagrant run list

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,5 +7,3 @@
 
 include_recipe 'jenkins-liatrio::default'
 include_recipe 'jenkins-liatrio::install_plugins'
-include_recipe 'jenkins-liatrio::create_creds'
-include_recipe 'jenkins-liatrio::job_seed_automation'
